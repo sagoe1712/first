60bf62a7f26bf085b3f202be033d7229118f7c83
Olayinka Sagoe <sagoe.olayinka@gmail.com>
2021-05-23 09:01:10 +0100

    parsing and result

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const fs = require('fs');
 
 const rqListener = (req, res) => {
 // console.log(req);
@@ -6,6 +7,7 @@ const rqListener = (req, res) => {
 // process.exit();
 
 const url = req.url;
+const method = req.method;
 if(url === '/'){
 res.setHeader('Content-Type', 'text/html');
 res.write('<html>');
@@ -16,19 +18,55 @@ res.write('</body>');
 res.write('</html>');
 return res.end();
 }
-if(url === '/login'){
+if(url === '/login' && method === 'GET'){
     res.setHeader('Content-Type', 'text/html');
 res.write('<html>');
 res.write('<head><title>My First Node Response Form</title></head>');
 res.write('<body>');
-res.write('username <input type="text"/>');
+res.write('<form action="/login" method="post">');
+res.write('username <input type="text" name="username"/>');
 res.write('<br/>');
-res.write('Password <input type="password"/>');
+res.write('Password <input type="password" name="password"/>');
 res.write('<br/>');
-res.write('<button>Login</button>');
+res.write('<button type="submit">Login</button>');
+res.write('</form>');
 res.write('</body>');
 res.write('</html>');
 return res.end();
+}
+if(url === '/dashboard'){
+    res.write('<html>');
+res.write('<head><title>Dashbord Yinka</title></head>');
+res.write('<body>');
+res.write('Hello ');
+res.write('</body>');
+res.write('</html>');
+return res.end();
+
+}
+if(url === '/login' && method === 'POST'){
+    const body = [];
+    req.on('data', (chunk) => {
+            body.push(chunk);
+    });
+    req.on('end',()=>{
+        const parsedBody = Buffer.concat(body).toString();
+        const username1 = parsedBody.split('=')[1];
+        const username = username1.split('&')[0];
+        const password = parsedBody.split('password=')[1];
+
+
+        if(username === 'yinka' && password === 'pass'){
+            res.statusCode = 302;
+            res.setHeader('location', 'dashboard');
+            return res.end();
+        }else{
+            res.statusCode = 401;
+            res.setHeader('location', 'login');
+            return res.end();
+        }
+
+    })
 }
 }
 
